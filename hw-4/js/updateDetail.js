06bfe037e6c4cb80cd6855521d60d8e229df93f9
irenehong213 <157193297+irenehong213@users.noreply.github.com>
2024-02-20 01:18:47 -0500

    Hw 4

---
+++ hw-4/js/updateDetail.js
@@ -0,0 +1,52 @@
+class Roll{
+    constructor(rollType, rollGlazing, packSize, basePrice)
+    {
+        this.type=rollType;
+        this.glazing=rollGlazing;
+        this.size=packSize;
+        this.basePrice=basePrice;
+
+    }
+}
+
+//new array for cart//
+const cart = [];
+
+//roll type from url//
+const queryString = window.location.search;
+const params = new URLSearchParams(queryString);
+const rollType = params.get("roll");
+
+//extract current roll's information//
+let selectedRoll = rolls[rollType];
+
+//update DOM elements//
+
+function render(){
+    let heading = document.querySelector("#detailheader");
+    let detailRollPic = document.querySelector("#productdetailphoto");
+    let basePrice = document.querySelector(".productprice")
+
+    //update heading to be <type> Cinnamon Roll//
+    heading.innerText = rollType + " Cinnamon Roll";
+
+    //update product page image//
+    detailRollPic.src = "./assets/products/" + selectedRoll.imageFile;
+
+    //update product detail base price//
+    basePrice.innerText = "$" + selectedRoll.basePrice;
+}
+
+//add roll to array cart//
+let cartButton=document.querySelector("#cartbutton");
+cartButton.addEventListener("click",addToCart);
+
+function addToCart(){
+    let glazingChange = document.querySelector("#glazingdd");
+    let glazingText = glazingChange.options[glazingChange.selectedIndex].text;
+    let rollChoice = new Roll(rollType, glazingText, Number(packChange.value), selectedRoll.basePrice);
+    cart.push(rollChoice);
+    console.log(cart);
+}
+
+render();
---
+++ hw-4/js/updatePrice.js
@@ -0,0 +1,49 @@
+
+let glazing = [
+    {glazing: 'Keep original', price: 0.0},
+    {glazing: 'Sugar milk', price: 0.0},
+    {glazing: 'Vanilla milk', price: 0.5},
+    {glazing: 'Double chocolate', price: 1.50}
+]
+
+let packsize = [
+    {size: 1, pricefactor: 1},
+    {size: 3, pricefactor: 3},
+    {size: 6, pricefactor: 5},
+    {size: 12, pricefactor: 10}
+]
+
+let glazingdrop = document.querySelector('#glazingdd');
+let packdrop = document.querySelector('#packsizedd');
+
+for (var i = 0; i < glazing.length; i++) {
+    var option = glazing[i];
+    var optionElement = document.createElement("option");
+    optionElement.textContent = option.glazing;
+    optionElement.value = option.price;
+    glazingdrop.appendChild(optionElement);    
+}
+
+for (var i = 0; i < packsize.length; i++) {
+    var option = packsize[i];
+    var optionElement = document.createElement("option");
+    optionElement.textContent = option.size;
+    optionElement.value = option.pricefactor;
+    packdrop.appendChild(optionElement);    
+}
+
+let glazingChange = document.querySelector('#glazingdd');
+glazingChange.addEventListener("change", dropdownChange);
+
+let packChange = document.querySelector('#packsizedd');
+packChange.addEventListener("change", dropdownChange);
+
+function dropdownChange() {
+    let glazeNew = Number(glazingChange.value);
+    let packNew = Number(packChange.value);
+    let price = document.querySelector('.productprice');
+    const basePrice = 2.49;
+    let calcPrice_rounded = ((basePrice + glazeNew) * packNew).toFixed(2);    
+    let calcPrice= ("$" + calcPrice_rounded);
+    price.innerText = calcPrice;
+}
